4ce19dafbf3ffd3288dd4de32aee95829ebbbd42
Karthik Raja <YOUR_GITHUB_EMAIL@gmail.com>
2026-07-25 16:02:16 +0530

    chnages hjssh

--- studiomathri/settings.py
+++ studiomathri/settings.py
@@ -57,10 +57,16 @@ TEMPLATES = [
     },
 ]
 
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
